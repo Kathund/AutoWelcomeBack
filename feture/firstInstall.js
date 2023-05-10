5dc77d6391d4c8aa79fdb14093edeee8575d2e04
Kath <55346310+Kathund@users.noreply.github.com>
2023-05-10 10:00:52 +0800

    Removed useless files

--- feture/firstInstall.js
+++
@@ -1,9 +0,0 @@
-import * as helperFunction from "../helperFunctions.js";
-
-let checked = false;
-register("step", () => {
-  if (checked) return;
-  checked = true;
-  if (!helperFunction.data.firstTimeMsg) return;
-  helperFunction.ShowFirstLoginMessage();
-}).setFps(5)
